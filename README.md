f5b73e8da7742f191a7304100694aa8d305d1f76
Aaron Suarez <aaron.j.suarez1@gmail.com>
2021-06-03 23:14:34 -0500

    Actually change the password

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Change WiFi Password
 
 This repository contains a small script used to change the wifi password. I set this up on a cron job so that the password on a certain network changes every day, such that my daughter has to do her chores before she receives the WiFi password.
+
+## Usage
+
+```bash
+python change_pass.py
+```
+
+## Credits
+Wordlist taken from https://gist.github.com/marswh/0e06461ebf3a7cc48761629af2f8f445
--- change_pass.py
+++ change_pass.py
@@ -1,11 +1,42 @@
 from dotenv import load_dotenv
 
 import os
+import random
 import requests
+import urllib.parse
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 load_dotenv()
 
-print(os.getenv('ROUTER_CREDENTIALS'))
-print(os.getenv('ROUTER_IP'))
-print(os.getenv('ROUTER_PORT'))
+# Get the Environment Variables we need
+creds = os.getenv('ROUTER_CREDENTIALS')
+ip = os.getenv('ROUTER_IP')
+port = os.getenv('ROUTER_PORT')
+url_plus_encoded_ssid = os.getenv("URL_PLUS_ENCODED_SSID")
+url_encoded_ssid = os.getenv("URL_ENCODED_SSID")
+double_encoded_old_password = urllib.parse.quote_plus(urllib.parse.quote_plus(os.getenv("OLD_PASSWORD")))
+model = os.getenv("MODEL_NUMBER")
+
+headers = {'Referer': f"https://{ip}:{port}/Main_Login.asp", 'Content-Type': 'application/x-www-form-urlencoded'}
+params = f"login_authorization={urllib.parse.quote(creds)}"
+
+s = requests.Session()
+r = s.post(f"https://{ip}:{port}/login.cgi", headers=headers, data=params, verify=False)
+
+words = open("wordlist.txt", "r").read().splitlines()
+new_password = random.choice(words).capitalize() + random.choice(words).capitalize()
+
+payload = (f"current_page=%2F&next_page=%2F&action_mode=apply_new&action_script=restart_wireless" +
+f"&action_wait=8&productid={model}&wps_enable=0&wsc_config_state=1&wl_ssid_org={url_plus_encoded_ssid}" +
+f"&wl_wpa_psk_org={double_encoded_old_password}&wl_auth_mode_orig=pskpsk2&wl_nmode_x=0&wps_band=0" +
+f"&wl_unit=0&wl_mfp=1&wl_subunit=-1&smart_connect_x=0&smart_connect_t=0&wl_ssid={url_encoded_ssid}" +
+f"&wl_auth_mode_x=pskpsk2&wl_crypto=aes&wl_wpa_psk={new_password}")
+
+headers = {'Referer': f"https://{ip}:{port}/device-map/router.asp", 'Content-Type': 'application/x-www-form-urlencoded'}
+
+r = s.post(f"https://{ip}:{port}/start_apply2.htm", headers=headers, data=payload, verify=False)
+
+print(f"NEW PASSWORD: {new_password}")
 
